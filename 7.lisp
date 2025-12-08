56061e9df4478db8f68094668dde89fbe871d558
Liam Pribis <liam.pribis@digiconsoles.com>
2025-12-08 17:08:50 +0000

    7 part 1 fucked

--- 7.lisp
+++ 7.lisp
@@ -18,9 +18,37 @@
                   (setf start-x x))))
     (values grid start-x)))
 
-(defun split-stream (stream-arr splitters))
 ; given stream-arr (list of bool) which is where the stream is, and splitters
 ; which is list of bool where the splitters are, determine what the new stream
 ; is. Also need to count then number of splits somehow
 
 (parse-grid (uiop:read-file-lines "7.example"))
+
+(defparameter *a* (make-array '(3 2) :initial-element 0))
+
+(defun row-slice (a row)
+  (make-array
+    (array-dimension a 1)
+    :displaced-to a
+    :displaced-index-offset (* row (array-dimension a 1))))
+
+(row-slice *a* 2)
+
+(defun elt-oob (seq i)
+  (if (and (<= 0 i) (> (length seq) i)) (elt seq i) nil))
+
+(defun split-single-row (streams splitter-row i)
+  (or
+    (and (elt streams i) (not (elt splitter-row i)))
+    (and (elt-oob streams (1- i)) (elt-oob splitter-row (1- i)))
+    (and (elt-oob streams (1+ i)) (elt-oob splitter-row (1+ i)))))
+    
+
+(defun split-stream (streams splitter-row)
+  (loop
+    for stream-p in streams
+    for i from 0
+    collect (split-single-row streams splitter-row i)))
+
+(split-stream '(nil nil t nil nil) '(nil nil t nil nil))
+; todo need to count the splits
